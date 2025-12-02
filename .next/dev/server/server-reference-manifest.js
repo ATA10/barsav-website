caf8fb114ff03ffbe8501c2fa3aabe4beaf89f9b
aniltaha <60629661+ATA10@users.noreply.github.com>
2025-12-02 21:45:45 +0300

    Remove node_modules and build files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sZ2ayztN1HZWgy3QQRMzpIGWeXAFGDQkC1sK19ybhMk=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_40ec9e97._.js",
-  "static/chunks/node_modules_7aed0e24._.js"
-],
-    source: "dynamic"
-});
