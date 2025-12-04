28aec878ca12f0f54d7fc8ec287d811807a60537
aniltaha <60629661+ATA10@users.noreply.github.com>
2025-12-04 20:26:53 +0300

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,28 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+ 
+// }
+
+// export default nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/barsav-website",
+  assetPrefix: "/barsav-website/",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
-  },
- 
+    unoptimized: true
+  }
 }
 
-export default nextConfig
+module.exports = nextConfig
