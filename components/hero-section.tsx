8a52f1e402d2f621a72220dfe27c1460ca37cada
aniltaha <60629661+ATA10@users.noreply.github.com>
2025-12-05 23:39:33 +0300

    12

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,6 +5,7 @@
   import Image from "next/image"
   import { useLanguage } from "@/contexts/language-context"
   import { getTranslation } from "@/lib/translations"
+  import ImageWithBasePath from "@/components/image-with-basepath"
 
   export default function HeroSection() {
     const { language } = useLanguage()
@@ -29,14 +30,15 @@
         </div>
 
         <div className="absolute inset-0 opacity-10">
-          <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg">
+          {/* <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg">
             <defs>
               <pattern id="grid" width="40" height="40" patternUnits="userSpaceOnUse">
                 <path d="M 40 0 L 0 0 0 40" fill="none" stroke="currentColor" strokeWidth="0.5" />
               </pattern>
             </defs>
             <rect width="100%" height="100%" fill="url(#grid)" className="text-blue-400" />
-          </svg>
+          </svg> */}
+          <ImageWithBasePath src="/a.jpg" alt="Barsav Teknoloji" fill className="object-cover" />
         </div>
 
         <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
